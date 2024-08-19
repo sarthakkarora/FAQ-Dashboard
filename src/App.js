b018d8ccb61e1af4e4a41ef8c34f95f25dbcc4e9
Sarthak Arora <sarthakk.arora1@gmail.com>
2024-08-19 21:16:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import FAQPage from "./pages/FAQPage";
+import React, { useState } from 'react';
+import FAQPage from './pages/FAQPage';
+import LoginPage from './pages/LoginPage';
 
-const App = () => (
-    <div className="app-container">
-        <FAQPage />
-    </div>
-);
+const App = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const handleLogin = () => {
+        setIsLoggedIn(true);
+    };
+
+    return (
+        <div className="app-container">
+            {isLoggedIn ? <FAQPage /> : <LoginPage onLogin={handleLogin} />}
+        </div>
+    );
+};
 
 export default App;
